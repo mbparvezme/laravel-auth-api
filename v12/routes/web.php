96602106fa2119c5615df077b6fc241cf2e29705
mbparvezme <mbparvezme@gmail.com>
2026-06-09 04:23:43 +0600

    Completed all tasks

--- v12/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::fallback(function () {
-    echo "<div style='text-align:center;user-select:none;padding-top:12%'>Access forbidden</div>";
-});
